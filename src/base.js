3181d37a2266702c02c6211ee06508558ed3ea9e
Mara Behles Dean <maraebehles@gmail.com>
2016-12-02 18:28:24 -0700

    add fish form

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyDXdrkk73xYRbiAKKnW8wb6TyfzKUNPHYI",
+  authDomain: "catch-of-the-day-7315d.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-7315d.firebaseio.com",
+});
+
+export default base;
--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -20,7 +20,7 @@ class AddFishForm extends React.Component {
       <form ref={(input) => this.fishForm =input } className="fish-edit" onSubmit={(e) => this.createFish(e)}>
         <input ref={(input) => this.name = input} type="text" placeholder="Fish Name" />
         <input ref={(input) => this.price = input} type="text" placeholder="Fish Price" />
-        <select ref={(input) => status.name = input} >
+        <select ref={(input) => this.status = input} >
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
